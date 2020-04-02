3dd5609f30c8546b4ddef92dbecbeed54af1d13d
Lucia Maj <38432852+luciamaj@users.noreply.github.com>
2020-04-02 17:00:16 +0200

    move cookie up

--- src/components/Piramide.vue
+++ src/components/Piramide.vue
@@ -13,8 +13,8 @@
                 </div>
             </div>
             <div class="cibo-container d-flex flex-wrap justify-content-center">
-                <template v-for="(cibo, idx) in food">
-                    <div v-bind:key="idx" class="fooditem">
+                <template v-for="(cibo) in food">
+                    <div v-on:click="moveFood(cibo.name)" v-bind:key="cibo.name" class="fooditem" :ref="cibo.name">
                         <img :src="'/static/piramide/cibo/' +  cibo.name + '.png'" class="food-image">
                         <img :src="'/static/piramide/cibo/' +  cibo.name + '.png'" class="food-image">
                     </div>
@@ -31,11 +31,27 @@ import { TimelineLite } from 'gsap'
 import gsap from 'gsap';
 import { Draggable } from 'gsap';
 import { data } from './../data/piramide.js'
+
+const timeline = new TimelineLite();
+var overlapThreshold = "50%";
  
 export default {
     data() {
         return {
             food: data.food,
+        }
+    },
+    mounted() {
+        //const { cookie } = this.$refs;
+        //timeline.to(cookie, 1, { x: 200, rotation: 90 });
+    },
+    methods: {
+        moveFood(element) {
+            console.log("THE ELEMENT CLICKED", element);
+            if(element == "cookie") {
+                const { cookie } = this.$refs;
+                timeline.to(cookie, 1, { y: -250 });
+            }
         }
     }
 }
