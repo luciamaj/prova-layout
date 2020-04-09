f469552433e12d03e3de252820289a49f8e448f7
SambiC <49269867+SambiC@users.noreply.github.com>
2020-04-09 16:54:00 +0200

    dish animation

--- src/components/PiattoVirtuale.vue
+++ src/components/PiattoVirtuale.vue
@@ -5,13 +5,13 @@
                 <div class="over"> <img src="../assets/cammello.jpg" alt=""></div>
                 <div class="row topPart">
                     <div class="col-lg-md-2 col-sm-1"> <button class="btn-control cancella"></button><div  class="btn-label">rimuovi</div></div>
-                    <div class="col-lg-md-8 col-sm-10"> 
+                    <div class="col-lg-md-8 col-sm-10" ref="areaVass"> 
                         <img src="/static/piatto_virtuale/vassoio.png" alt="vassoio">
                         <div class="over-vassoio"> 
-                            <div class="dish-1" id="dish-1"><img src="../assets/cammello.jpg" alt=""></div>
-                            <div class="dish-2"></div>
-                            <div class="dish-3"></div>
-                            <div class="dish-4"></div>
+                            <div class="dish-1" id="dish-1" ref="dish1"><img src="../assets/cammello.jpg" alt=""></div>
+                            <div class="dish-2" id="dish-2" ref="dish2"></div>
+                            <div class="dish-3" id="dish-3" ref="dish3"></div>
+                            <div class="dish-4" id="dish-4" ref="dish4"></div>
                         </div>
                         <h3>Scegli fra le 25 pietanze proposte</h3>
                     </div>
@@ -20,7 +20,7 @@
                 <div class="foodList">
                     <slick ref="slick" :options="slickOptions" :arrows="true" >
                         <template v-for="(piatto,index) in food" >
-                            <div class="food-item" :id="piatto.id" v-bind:key="(piatto,index)"  v-on:click="selected(piatto)"> 
+                            <div class="food-item" :ref="piatto.id" :id="piatto.id" v-bind:key="(piatto,index)"  v-on:click="selected(piatto)"> 
                                 <div class="mainName">{{piatto.name}}</div>
                                 <div class="secondName" v-if="piatto.name2!=null">{{piatto.name2}}</div>
                             </div>
@@ -41,10 +41,11 @@ import gsap from 'gsap';
 import { Draggable } from 'gsap';
 import { data } from './../data/piatto.js'
 import Slick from 'vue-slick';
-
+import jquery from 'jquery';
+let $ = jquery;
 
 const timeline = new TimelineLite();
-let dimensions = {section1: {}, section2: {}, section3: {}, section4: {}, section5: {}, section6: {}}
+
 
 export default {
     components: { Slick },
@@ -78,34 +79,83 @@ export default {
                 if( !found){
                 this.foodSelected.push(piatto.id);
                 selected.style.border="solid 1px white";
-                  selected.style.animationName="example";
-                  dish.style.animationName="dish-drop";
+                  //selected.style.animationName="example";
+                this.moveFood(piatto);
                 }
             }
-
-               if(element.name == "cookie") {
-                const { cookie, cookie1, cookie2, ciboContainer } = this.$refs;
-                let dims = ciboContainer.getBoundingClientRect();
-                console.log("DIMSSS", dims);
-                let firstDest = dimensions[element.firstDestination];
-                let secondDest = dimensions[element.secondDestination];
-
-                console.log(firstDest, secondDest);
-                
-                document.getElementById("cookie2").style.position = "absolute";
-                document.getElementById("cookie2").style.zIndex = 999;
-
-                
-                timeline.to(cookie1, 1, { y: -200, x: -30,  scale: 0.5});
-                timeline.to(cookie2, 1, { y: 200, x: 50, scale: 0.5 });
-            }
-
             console.log("lunghezza "+this.foodSelected.length);
             console.log(this.foodSelected);
             
         },
 
-         
+        moveFood(element) {
+            console.log("THE ELEMENT CLICKED", element.id);
+            let dishDiv= document.getElementById(element.id);
+            let item=element.id;
+            let item_ref=this.$refs[item];
+
+                let dimsDish;
+                let dimOrizz= this.$refs.areaVass.getBoundingClientRect();
+                let dimItem= dishDiv.getBoundingClientRect();
+
+                let deltax;
+                let deltay;
+               
+                /*console.log("DIMSSS orizz", dimOrizz);*/
+                 //console.log("DIMSSS dish", dimsDish2); 
+                 console.log("DIMSSS divtomove", dimItem);
+                
+                console.log("deltay", deltay); 
+
+                dishDiv.style.backgroundColor="red";
+                switch (this.foodSelected.length) {
+                    case 1:
+                        dimsDish = this.$refs.dish1.getBoundingClientRect();
+                        deltax= dimsDish.x-dimItem.x;
+                        deltay=dimsDish.y-dimItem.y;
+                        break;
+                
+                    case 2:
+                        dimsDish = this.$refs.dish2.getBoundingClientRect();
+                        deltax= dimsDish.x-dimItem.x;
+                        deltay=dimsDish.y-dimItem.y;
+                           
+                        break;
+                    case 3:
+                        dimsDish = this.$refs.dish3.getBoundingClientRect();
+                        deltax= dimsDish.x-dimItem.x;
+                        deltay=dimsDish.y-dimItem.y;
+                        break;
+
+                    case 4:
+                        dimsDish = this.$refs.dish4.getBoundingClientRect();
+                        deltax= dimsDish.x-dimItem.x;
+                        deltay=dimsDish.y-dimItem.y;
+                        break;
+                
+                    default:
+                        break;
+                }
+                
+                timeline.to(item_ref, 1, { y: deltay, x: deltax,  scale: 0.95});
+                setTimeout(function(){
+                    
+                    let myDiv2Para = $('#'+element.id).detach();
+                    myDiv2Para.appendTo('#dish-1');
+                    dishDiv.style.transform="translate(50%, 0)";
+
+                }, 1000);
+                
+                    
+                 //dishDiv.appendChild(this.$refs.dish1);
+
+                  /*var photo = document.getElementById("cookie1");
+                  photo.id  = "cookie1moved";
+                  document.getElementById("piramide-container").appendChild(photo);
+
+                  timeline.to(cookie2, 1, { y: -250, x: 50, scale: 0.5 });*/
+           
+        },
       
     }
 }
