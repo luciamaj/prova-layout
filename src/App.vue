9ed3e7c8013049fcbef2bc3f3af4505fb69002a1
SambiC <49269867+SambiC@users.noreply.github.com>
2020-04-15 00:15:16 +0200

    aggiunta pagine finali piatto virtuale

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">  
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans+Condensed&family=Roboto+Condensed:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap" rel="stylesheet">
     <router-view></router-view>
   </div>
 </template>
