3639992a500deeef31dbeb05b50f99220562d4a9
Lucia Maj <38432852+luciamaj@users.noreply.github.com>
2020-04-14 13:04:00 +0200

    fixed reset

--- src/components/Piramide/Clone.vue
+++ src/components/Piramide/Clone.vue
@@ -3,7 +3,7 @@
         <div class="piramide-cont">
             <div class="section-container">
                 <center><img id="cane" src="/static/piramide/piramidi.png" alt="Responsive image"></center>
-                <div class="topright"></div>
+                <div v-on:click="resetPositions" class="topright"></div>
                 <div ref="right6" id="right6" class="right6 blocks rightblock"></div>
                 <div ref="right5" id="right5" class="right5 blocks rightblock"></div>
                 <div ref="right4" id="right4" class="right4 blocks rightblock"></div>
@@ -27,7 +27,7 @@
                             <template v-for="(cibo, index) in food">
                                 <div v-bind:key="(cibo, index)" class="tile-wrapper">
                                     <div :id="cibo.name" class="tile">
-                                        <img :src="'/static/piramide/cibo/' +  cibo.name + '.png'" :ref="cibo.name" :id="cibo.name" :class="cibo.name">
+                                        <img :src="'/static/piramide/cibo/' +  cibo.name + '.png'" :ref="cibo.name" :class="cibo.name">
                                     </div>
                                 </div>
                             </template>
@@ -75,7 +75,8 @@ export default {
                 slidesToShow: 7,
                 slidesToScroll: 2,
             },
-            ratio: 0
+            ratio: 0,
+            scopes: []
         }
     },
     mounted() {
@@ -115,21 +116,38 @@ export default {
                 console.log("the offset", offset);
 
                 var scope = {
-                    clone1  : element.clone().addClass("clone").addClass("clone1").prependTo(container),
-                    clone2  : element.clone().addClass("clone").addClass("clone2").prependTo(container),
+                    name    : element.attr('id'),
+                    clone1  : element.clone().addClass("clone").addClass("clone1").prop("id", (element.prop('id') + "clone1")).prependTo(container),
+                    clone2  : element.clone().addClass("clone").addClass("clone2").prop("id", (element.prop('id') + "clone2")).prependTo(container),
                     element : element,
                     wrapper : wrapper,
+                    offset  : element.position(),
                     width   : wrapper.outerWidth(),
                     get x() { return getPosition(wrapper, offset, container).x; },
-                    get y() { return getPosition(wrapper, offset, container).y; }
+                    get y() { return getPosition(wrapper, offset, container).y; },
+                    moved   : false,
+                    initial : null,
                 };
 
-                console.log("THE ELEMENT", element.attr('id'));
+                that.scopes.push(scope);
 
-                const timeline = new TimelineLite();
+                console.log("THE ELEMENT", scope.name);
+
+                const timeline = new TimelineLite({onComplete: function() {
+                    console.log("COMPLETED TIMELINE");
+                }});
 
                 element.click(function() {
                     if (!(element.hasClass('cloned'))) {
+                        console.log("THAT", that);
+                        let scopeToChange = that.scopes.find(scope => scope.name == element.attr('id'));
+                        if (scopeToChange) {
+                            console.log("CHANGING TO TRUEEEEEEEEEEEEEEEE")
+                            scopeToChange.moved = true;
+                        }
+
+
+                        console.log("THE INITIAL POSITION", scope.clone1)
                         let foodElement = that.food.find(cibo => cibo.name == element.attr('id'));
                         let firstDestName = foodElement.destinations[0].dest;
                         let secondDestName = foodElement.destinations[1].dest;
@@ -158,14 +176,20 @@ export default {
                         console.log("offs", offset.left);
                         console.log("x", scope.x);
 
+                        that.initial = scope.clone1.offset();
+
                         TweenLite.set(scope.element, { border: "solid 2px white" });
                         TweenLite.set(scope.clone1, { x: scope.x+(scope.element.position().left)-offset.left, y: scope.y+1, autoAlpha: 1});
                         TweenLite.set(scope.clone2, { x: scope.x+(scope.element.position().left)-offset.left, y: scope.y+1, autoAlpha: 1});
+
                         element.addClass("cloned");
 
-                        console.log("cl", scope.clone1.position().left);
+
+
                         timeline.to(scope.clone1, 1, {x: dimRect1.x, top: yDish1, scale: 0.3});
                         timeline.to(scope.clone2, 1, {x: dimRect2.x, top: yDish2, scale: 0.3});
+                        scope.clone1.addClass('moved');
+                        scope.clone2.addClass('moved');
                     }                   
                 });
 
@@ -173,6 +197,10 @@ export default {
                     console.log("ho cliccato sui cloni");
                 })
             });
+        },
+        positionElement (scope) {
+            TweenLite.set(scope.clone1, { x: scope.x+(scope.element.position().left)-offset.left, y: scope.y+1, autoAlpha: 1});
+            TweenLite.set(scope.clone2, { x: scope.x+(scope.element.position().left)-offset.left, y: scope.y+1, autoAlpha: 1});
         },
         setCarousel(){
             let content = $("#tile-container");
@@ -232,6 +260,34 @@ export default {
                     console.log("complete");
                     isMoving = false;
                 }});
+            }
+        },
+        resetPositions() {
+            //console.log("I CLICKED ON RESET POSITIONS", this.scopes);
+            let cloned = $('.moved');
+            var container = $("#clone-container");
+
+            if (cloned.length > 0) {
+                console.log("THE CLONED", cloned);
+
+                for(let scope of this.scopes) {
+                    console.log("THE EL", scope);
+                    if (scope.moved == true) {
+                        let element = $('#' + scope.name);
+                        element.removeClass('cloned')
+                        var offset  = element.position();
+
+                        console.log("SONO QUIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII", element);
+                        if (this.initial) {
+                            console.log("INITIALLLLLLLLL", this.initial);
+                            TweenLite.set(scope.element, { border: "solid 0px black" });
+                            TweenLite.set(scope.clone1, { top: 0, left: 0, autoAlpha: 0, scale: 1});
+                            TweenLite.set(scope.clone2, { top: 0, left: 0, autoAlpha: 0, scale: 1});
+                        }
+                    }
+                }
+            } else {
+                console.log("nothing to reset");
             }
         }
     }
