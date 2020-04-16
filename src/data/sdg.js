75914e7015abdc1694b6c5dfb04adcfd4c883354
SambiC <49269867+SambiC@users.noreply.github.com>
2020-04-16 19:52:05 +0200

    Aggiunta pagina Cibo & SDG

---
+++ src/data/sdg.js
@@ -0,0 +1,24 @@
+var data = {
+    obiettivi: [
+        {id:"1" ,name: "sconfiggere la povertà", name2:null, text1:"pippo", text2:null, color:"#cd0404" },
+        {id: "2", name: "sconfiggere la fame", name2:null, text1:"pluto", text2:null, color:"#f54141"},
+        {id: "3",name: "salute e benessere", name2:null,text1:"paperino", text2:null, color:"#0aa746"},
+        {id: "4", name: "istruzione di qualità", name2:null,text1:"minni", text2:null, color:"#dc1b1b" },
+        {id: "5",name: "parità di genere ", name2:null,text1:null, text2:null, color:"#da410d"},
+        {id: "6",name: "acqua pulita e servizi igienico sanitari",name2:null,text1:null, text2:null, color:"#00bdd6" },
+        {id: "7",name: "energia pulita e accessibile", name2:null, text1:null, text2:null, color:"#f7bd00"},
+        {id: "8",name: "lavoro dignitoso e crescita economica",name2:null,text1:null, text2:null, color:"#b70262" },
+        {id: "9",name: "imprese, innovazione e infrastrutture",name2:null,text1:null, text2:null, color:"#ff7c00" },
+        {id: "10",name: "ridurre le diseguaglianze", name2:null,text1:null, text2:null, color:"#ff16b1"},
+        {id: "11",name: "città e comunità sostenibili",name2:null,text1:null, text2:null, color:"#e39803" },
+        {id: "12",name: "consumo e produzione responsabili", name2:null,text1:null, text2:null, color:"#bb8820"},
+        {id: "13",name: "lotta contro il cambiamento climatico", name2:null,text1:null, text2:null, color:"#076b44" },
+        {id: "14",name: "vita sott'acqua", name2:null,text1:null, text2:null, color:"#009cd2"},
+        {id: "15",name: "vita sulla terra", name2:null,text1:null, text2:null, color:"#43a927"},
+        {id: "16",name: "pace, giustizia e istituzioni solide", name2:null,text1:null, text2:null, color:"#176cbd"},
+        {id: "17",name: "partnership per gli obiettivi", name2:null,text1:null, text2:null, color:"#23335d"},
+        
+    ]
+}
+
+exports.data = data;    
